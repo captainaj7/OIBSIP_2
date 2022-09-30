f74312bb2cc74d7f2c2ae119fd51e5037223cf29
Ajay <91454482+captainaj7@users.noreply.github.com>
2022-09-30 22:20:12 +0530

    Create Start.java

---
+++ Online Reservation System/Start.java
@@ -0,0 +1,16 @@
+package reservation;
+
+/**
+ *
+ * @author ARUNIMA
+ */
+public class Reservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
